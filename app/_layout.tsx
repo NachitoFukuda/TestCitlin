b8d5abf8ad0a653b5891227691c9e77f4ed29b0d
orenonamae <iii932340@gmail.com>
2025-09-06 22:50:34 +0900

    Update CardDesign.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    DancingScript_700Bold: require('../assets/fonts/DancingScript-Bold.ttf'),
   });
 
   useEffect(() => {
